c705fe1a6ad243074f508748548f07081913de23
Zamberd <petrovitalia@ya.ru>
2017-05-29 02:09:21 +0400

    All test task

---
+++ json.js
@@ -0,0 +1,408 @@
+var menuAll = [
+				{
+					"menu_id": 1,
+					"menu_title": "Menu item 1",
+					"menu_icon": "http://digiline.co/test_task/img/menu/1.png",
+					"menu_items": [
+						{
+							"category_id": 1,
+							"category_title": "Category item 1",
+							"category_icon": "http://digiline.co/test_task/img/category/1.png"
+						},
+						{
+							"category_id": 2,
+							"category_title": "Category item 2",
+							"category_icon": "http://digiline.co/test_task/img/category/2.png"
+						},
+						{
+							"category_id": 3,
+							"category_title": "Category item 3",
+							"category_icon": "http://digiline.co/test_task/img/category/3.png"
+						},
+						{
+							"category_id": 4,
+							"category_title": "Category item 4",
+							"category_icon": "http://digiline.co/test_task/img/category/4.png"
+						},
+						{
+							"category_id": 5,
+							"category_title": "Category item 5",
+							"category_icon": "http://digiline.co/test_task/img/category/5.png"
+						},
+						{
+							"category_id": 6,
+							"category_title": "Category item 6",
+							"category_icon": "http://digiline.co/test_task/img/category/6.png"
+						},
+						{
+							"category_id": 7,
+							"category_title": "Category item 7",
+							"category_icon": "http://digiline.co/test_task/img/category/7.png"
+						},
+						{
+							"category_id": 8,
+							"category_title": "Category item 8",
+							"category_icon": "http://digiline.co/test_task/img/category/8.png"
+						},
+						{
+							"category_id": 9,
+							"category_title": "Category item 9",
+							"category_icon": "http://digiline.co/test_task/img/category/9.png"
+						},
+						{
+							"category_id": 10,
+							"category_title": "Category item 10",
+							"category_icon": "http://digiline.co/test_task/img/category/10.png"
+						},
+						{
+							"category_id": 11,
+							"category_title": "Category item 11",
+							"category_icon": "http://digiline.co/test_task/img/category/11.png"
+						},
+						{
+							"category_id": 12,
+							"category_title": "Category item 12",
+							"category_icon": "http://digiline.co/test_task/img/category/12.png"
+						}
+					]
+				},
+				{
+					"menu_id": 2,
+					"menu_title": "Menu item 2",
+					"menu_icon": "http://digiline.co/test_task/img/menu/2.png",
+					"menu_items": [
+						{
+							"category_id": 1,
+							"category_title": "Category item 1",
+							"category_icon": "http://digiline.co/test_task/img/category/1.png"
+						},
+						{
+							"category_id": 2,
+							"category_title": "Category item 2",
+							"category_icon": "http://digiline.co/test_task/img/category/2.png"
+						},
+						{
+							"category_id": 4,
+							"category_title": "Category item 4",
+							"category_icon": "http://digiline.co/test_task/img/category/4.png"
+						},
+						{
+							"category_id": 5,
+							"category_title": "Category item 5",
+							"category_icon": "http://digiline.co/test_task/img/category/5.png"
+						},
+						{
+							"category_id": 8,
+							"category_title": "Category item 8",
+							"category_icon": "http://digiline.co/test_task/img/category/8.png"
+						},
+						{
+							"category_id": 9,
+							"category_title": "Category item 9",
+							"category_icon": "http://digiline.co/test_task/img/category/9.png"
+						},
+						{
+							"category_id": 10,
+							"category_title": "Category item 10",
+							"category_icon": "http://digiline.co/test_task/img/category/10.png"
+						},
+						{
+							"category_id": 12,
+							"category_title": "Category item 10",
+							"category_icon": "http://digiline.co/test_task/img/category/12.png"
+						}
+					]
+				},
+				{
+					"menu_id": 3,
+					"menu_title": "Menu item 3",
+					"menu_icon": "http://digiline.co/test_task/img/menu/3.png",
+					"menu_items": []
+				},
+				{
+					"menu_id": 4,
+					"menu_title": "Menu item 4",
+					"menu_icon": "http://digiline.co/test_task/img/menu/4.png",
+					"menu_items":[]
+				},
+				{
+					"menu_id": 5,
+					"menu_title": "Menu item 5",
+					"menu_icon": "http://digiline.co/test_task/img/menu/5.png",
+					"menu_items":[]
+				},
+				{
+					"menu_id": 6,
+					"menu_title": "Menu item 6",
+					"menu_icon": "http://digiline.co/test_task/img/menu/6.png",
+					"menu_items":[]
+				},
+				{
+					"menu_id": 7,
+					"menu_title": "Menu item 7",
+					"menu_icon": "http://digiline.co/test_task/img/menu/7.png",
+					"menu_items":[]
+				}
+];
+var category_1 = [
+	{
+		"id": 1,
+		"number": "1",
+		"title": "item 1"
+	},
+	{
+		"id": 2,
+		"number": "2",
+		"title": "item 3"
+	},
+	{
+		"id": 3,
+		"number": "3",
+		"title": "item 3"
+	},
+	{
+		"id": 4,
+		"number": "4",
+		"title": "item 4"
+	},
+	{
+		"id": 5,
+		"number": "5",
+		"title": "item 5"
+	},
+	{
+		"id": 6,
+		"number": "6",
+		"title": "item 6"
+	},
+	{
+		"id": 7,
+		"number": "7",
+		"title": "item 7"
+	},
+	{
+		"id": 8,
+		"number": "8",
+		"title": "item 8"
+	},
+	{
+		"id": 9,
+		"number": "9",
+		"title": "item 9"
+	},
+	{
+		"id": 10,
+		"number": "10",
+		"title": "item 10"
+	},
+	{
+		"id": 11,
+		"number": "11",
+		"title": "item 11"
+	},
+	{
+		"id": 12,
+		"number": "12",
+		"title": "item 12"
+	},
+	{
+		"id": 13,
+		"number": "13",
+		"title": "item 13"
+	},
+	{
+		"id": 10,
+		"number": "10",
+		"title": "item 10"
+	},
+	{
+		"id": 11,
+		"number": "11",
+		"title": "item 11"
+	},
+	{
+		"id": 12,
+		"number": "12",
+		"title": "item 12"
+	},
+	{
+		"id": 13,
+		"number": "13",
+		"title": "item 13"
+	}
+];
+var category_2 = [
+	{
+		"id": 1,
+		"number": "1",
+		"title": "item 1"
+	},
+	{
+		"id": 3,
+		"number": "3",
+		"title": "item 3"
+	},
+	{
+		"id": 4,
+		"number": "4",
+		"title": "item 4"
+	},
+	{
+		"id": 7,
+		"number": "7",
+		"title": "item 7"
+	},
+	{
+		"id": 8,
+		"number": "8",
+		"title": "item 8"
+	},
+	{
+		"id": 9,
+		"number": "9",
+		"title": "item 9"
+	},
+	{
+		"id": 10,
+		"number": "10",
+		"title": "item 10"
+	},
+	{
+		"id": 12,
+		"number": "12",
+		"title": "item 12"
+	},
+	{
+		"id": 13,
+		"number": "13",
+		"title": "item 13"
+	}
+];
+var category_3 = [
+	{
+		"id": 2,
+		"number": "2",
+		"title": "item 3"
+	},
+	{
+		"id": 3,
+		"number": "3",
+		"title": "item 3"
+	},
+	{
+		"id": 4,
+		"number": "4",
+		"title": "item 4"
+	},
+	{
+		"id": 5,
+		"number": "5",
+		"title": "item 5"
+	},
+	{
+		"id": 6,
+		"number": "6",
+		"title": "item 6"
+	},
+	{
+		"id": 7,
+		"number": "7",
+		"title": "item 7"
+	},
+	{
+		"id": 8,
+		"number": "8",
+		"title": "item 8"
+	},
+	{
+		"id": 10,
+		"number": "10",
+		"title": "item 10"
+	},
+	{
+		"id": 11,
+		"number": "11",
+		"title": "item 11"
+	},
+	{
+		"id": 13,
+		"number": "13",
+		"title": "item 13"
+	}
+];
+var category_4 = [
+	{
+		"id": 1,
+		"number": "1",
+		"title": "item 1"
+	},
+	{
+		"id": 2,
+		"number": "2",
+		"title": "item 3"
+	},
+	{
+		"id": 3,
+		"number": "3",
+		"title": "item 3"
+	},
+	{
+		"id": 11,
+		"number": "11",
+		"title": "item 11"
+	},
+	{
+		"id": 12,
+		"number": "12",
+		"title": "item 12"
+	},
+	{
+		"id": 13,
+		"number": "13",
+		"title": "item 13"
+	}
+];
+var category_5 = [
+	{
+		"id": 1,
+		"number": "1",
+		"title": "item 1"
+	},
+	{
+		"id": 2,
+		"number": "2",
+		"title": "item 3"
+	},
+	{
+		"id": 3,
+		"number": "3",
+		"title": "item 3"
+	},
+	{
+		"id": 4,
+		"number": "4",
+		"title": "item 4"
+	},
+	{
+		"id": 7,
+		"number": "7",
+		"title": "item 7"
+	},
+	{
+		"id": 8,
+		"number": "8",
+		"title": "item 8"
+	},
+	{
+		"id": 9,
+		"number": "9",
+		"title": "item 9"
+	},
+	{
+		"id": 10,
+		"number": "10",
+		"title": "item 10"
+	}
+];
+
+var category = [category_1, category_2, category_3, category_4, category_5];
